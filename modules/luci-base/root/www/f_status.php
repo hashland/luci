82996d0e742cfb6286c87fef2765077711c6e4b4
Julian Haupt <julian.haupt@hauptmedia.de>
2018-11-08 15:09:59 +0100

    add some glue for upgrading from scripta

---
+++ modules/luci-base/root/www/f_status.php
@@ -0,0 +1,7 @@
+{
+	"info": [{"type": "success", "text": "Your miner was upgraded. &#128077;<br>Please refresh your browser now."}],
+        "status": {
+          "minerDown": true,
+          "minerUp": false
+        }
+}
